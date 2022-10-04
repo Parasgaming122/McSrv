f60ea2d05923a119e13f5d14a1fff1f5b24259a9
Parasgaming122 <98590880+Parasgaming122@users.noreply.github.com>
2022-10-04 16:48:16 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Xms6G -Xmx6G -XX:+UseG1GC -jar spigot-1.17.1.jar nogui
+wget https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
+java -Xms6G -Xmx6G -XX:+UseG1GC -jar spigot-1.8.8-R0.1-SNAPSHOT-latest.jar nogui
