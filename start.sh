17ea2fc7659f196598005dc945e3728994cd6e1e
Parasgaming122 <98590880+Parasgaming122@users.noreply.github.com>
2022-12-15 18:24:12 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-if test ! -e spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
-then wget https://cdn.getbukkit.org/spigot/spigot-1.8.8-R0.1-SNAPSHOT-latest.jar
+if test ! -e spigot-1.16.5.jar #put the name of the file here
+then wget https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar #put the link of your file here 
 fi
 if test ! -e eula.txt
 then echo "eula=true" > eula.txt
 fi
-ssh -o StrictHostKeyChecking=no -R $RANDOM:localhost:25565 serveo.net &
+fi
+if test ! -e server.properties
+then echo "server-port=34501" > server.properties #for if u wanna have a custom port change 25565 to ur desired port
+fi
+ssh -o StrictHostKeyChecking=no -R $RANDOM:localhost:34501 serveo.net & 
 echo "Starting Minecraft server… will take a while; please wait."
-java -Xms6G -Xmx6G -XX:+UseG1GC -jar spigot-1.8.8-R0.1-SNAPSHOT-latest.jar nogui
+java -Xms6G -Xmx6G -XX:+UseG1GC -jar spigot-1.16.5.jar nogui
