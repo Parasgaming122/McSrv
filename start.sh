3d99b282e62dc6ca0362d3697f9b73d938570182
Parasgaming122 <98590880+Parasgaming122@users.noreply.github.com>
2022-12-16 13:30:39 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,5 @@ fi
 if test ! -e server.properties
 then echo "server-port=34501" > server.properties #for if u wanna have a custom port change 25565 to ur desired port
 fi
-ssh -o StrictHostKeyChecking=no -R $RANDOM:localhost:34501 serveo.net & 
 echo "Starting Minecraft server… will take a while; please wait."
-java -Xms6G -Xmx6G -XX:+UseG1GC -jar spigot-1.16.5.jar nogui
+sh startscript.sh
