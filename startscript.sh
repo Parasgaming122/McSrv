cf7e33195eb5e9046efe38a89242ed817b2ebd3c
Parasgaming122 <98590880+Parasgaming122@users.noreply.github.com>
2022-12-16 13:29:48 +0500

    Update startscript.sh

--- startscript.sh
+++ startscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true
 do
-java -Xmx1G -Xms7G -jar craftbukkit.jar
+java -Xmx1G -Xms7G -jar spigot-1.16.5.jar
 echo "If you want to completely stop the server process now, press Ctrl+C before
 the time is up!"
 echo "Rebooting in:"
